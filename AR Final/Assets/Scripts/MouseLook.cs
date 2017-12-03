43216159da9ce477f4e43de6c2dd00903799d330
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-12-03 17:25:39 -0500

    Added Unity project and edited gitignore

---
+++ AR Final/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var myCam = Camera.main.transform;
+
+			//rotate camera based on mouse delta speed
+		myCam.Rotate (-Input.GetAxis ("Mouse Y"), Input.GetAxis ("Mouse X"), 0f);
+
+			//unroll our camera
+		myCam.localEulerAngles = new Vector3 (myCam.localEulerAngles.x, myCam.localEulerAngles.y, 0f); 
+
+	}
+}
